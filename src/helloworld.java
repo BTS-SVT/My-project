e25321f566e8a6b645135dfb595104d7a8af965d
BTS-SVT <bts279608@gmail.com>
2025-02-27 18:29:43 +0100

    third commit.java

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,5 +3,6 @@ public class helloworld {
         System.out.println("Hello World");
         System.out.println("I am AnnMaria");
         System.out.println("I am going to create my java program here");
+        System.out.println("I am a beginner in JAVA");
     }
 }
